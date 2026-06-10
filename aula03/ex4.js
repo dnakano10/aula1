0f081c62e80b6eab04aa6c68216f6edf1b838d89
dnakano10 <eduardaayuminakano@gmail.com>
2026-06-10 14:50:40 -0300

    transf. com map

---
+++ aula03/ex4.js
@@ -0,0 +1,26 @@
+//Declare um array de pelo menos 5 alunos com id, nome e nota. Use .map() para gerar um novo array onde cada objeto tenha nome, nota e uma propriedade situacao com o valor "Aprovado" ou "Reprovado" conforme a nota seja maior ou igual a 7. Exiba o resultado.
+
+const input = require('readline-sync');
+
+let alunos = [
+    {id: 1, nome: "Alice", nota: 8},
+    {id: 2, nome: "Maria", nota: 6},
+    {id: 3, nome: "Samuel", nota: 7},
+    {id: 4, nome: "Alana", nota: 5},
+    {id: 5, nome: "João", nota: 9}
+];
+
+let notas = alunos.map(aluno => {
+    return {
+        nome: aluno.nome,
+        nota: aluno.nota,
+        situacao: aluno.nota >= 7 ? "Aprovado" : "Reprovado"
+        };
+    }
+);
+
+console.log("Situação dos alunos:");
+notas.forEach(aluno => {
+    console.log(`- ${aluno.nome}: Nota ${aluno.nota} - ${aluno.situacao}`);
+    }
+);
