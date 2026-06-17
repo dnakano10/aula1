92a0d7f477cee482e0e46026d99c4f289792240e
dnakano10 <eduardaayuminakano@gmail.com>
2026-06-17 10:31:44 -0300

    lista01 - exercicios

--- lista01/ex15.js
+++ lista01/ex15.js
@@ -1,5 +1,44 @@
 //15 – Catálogo de produtos. Crie um array com 5 produtos (cada um com id, nome e preco). Em seguida:
+const input = require('readline-sync');
+
+const pd = [
+  { id: 1, nome: "produto1", preco: 50 },
+  { id: 2, nome: "produto2", preco: 150 },
+  { id: 3, nome: "produto3", preco: 200 },
+  { id: 4, nome: "produto4", preco: 80 },
+  { id: 5, nome: "produto5", preco: 120 }
+];
+
+
 //a) Use .find() para buscar e exibir o produto de id 3.
+const idDig = input.questionInt("Digite o ID do produto que deseja buscar: ");
+
+const pdenc = pd.find(p => p.id === idDig);
+if (pdenc) {
+  console.log(`produto encontrado: ID: ${pdenc.id}, nome: ${pdenc.nome}, preco: R$ ${pdenc.preco.toFixed(2)}`);
+} else {
+  console.log(`produto com ID ${idDig} não encontrado.`);
+}
+
+
 //b) Use .filter() para listar os produtos com preço acima de 100.
+const pdacima = pd.filter(p => p.preco > 100);
+console.log("produtos com preco acima de 100:");
+pdacima.forEach(p => {
+    console.log(`- ${p.nome}: R$ ${p.preco.toFixed(2)}`);
+});
+
+
 //c) Use .map() para criar um array só com os nomes dos produtos.
+const nomespd = pd.map(p => p.nome);
+console.log("nome dos produtos:");
+nomespd.forEach(nome => {
+    console.log(`- ${nome}`);
+});
+
+
 //d) Use .forEach() para exibir cada produto formatado.
+console.log("catálogo de produtos:");
+pd.forEach(p => {
+    console.log(`ID: ${p.id}, nome: ${p.nome}, preco: R$ ${p.preco.toFixed(2)}`);
+});
--- lista01/ex16.js
+++ lista01/ex16.js
@@ -1,1 +1,25 @@
 //16 – Boletim da turma (Desafio). Crie um array com pelo menos 5 alunos (com nome e nota). Use .map() para gerar um novo array onde cada aluno ganhe a propriedade situacao ("Aprovado" se nota ≥ 7, senão "Reprovado"). Depois, use .filter() para contar quantos foram aprovados e exiba a média geral da turma.
+const input = require('readline-sync');
+
+const al = [
+    { nome: "aluno_1", nota: 8 },
+    { nome: "aluno_2", nota: 6 },
+    { nome: "aluno_3", nota: 7 },
+    { nome: "aluno_4", nota: 5 },
+    { nome: "aluno_5", nota: 9 }
+]
+
+const situa = al.map(aluno => {
+    return {
+        nome: aluno.nome,
+        nota: aluno.nota,
+        situacao: aluno.nota >= 7 ? "Aprovado" : "Reprovado"
+    };
+});
+
+const ap = situa.filter(aluno => aluno.situacao === "Aprovado");
+const media = al.reduce((acc, aluno) => acc + aluno.nota, 0) / al.length;
+
+console.log("Alunos com situação:", situa);
+console.log("Número de alunos aprovados:", ap.length);
+console.log("Média geral da turma:", media.toFixed(2));
